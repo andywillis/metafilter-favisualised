ba28ed7de5e34f8a5674b42e157a52357ad9f889
Andy Willis <github@awillis.fastmail.fm>
2017-09-23 08:30:18 +0100

    Added new sites

--- content/index.js
+++ content/index.js
@@ -1,86 +1,48 @@
 (function () {
 
-  let timer;
-
-  function qs(selector) {
-    return document.querySelector(selector);
+  function qs(selector, context = document) {
+    return context.querySelector(selector);
   }
 
-  function qsa(selector) {
-    return document.querySelectorAll(selector);
+  function qsa(selector, context = document) {
+    return context.querySelectorAll(selector);
   }
 
-  function handleError(error) {
-    console.log(error);
+  function getFavouriteTotal(comment) {
+    const link = qs('span[id^="favcnt"] a', comment);
+    return link ? link.textContent.match(/^\d+/)[0] : 0;
   }
 
-  function showPopup(names, e) {
-    const popup = qs('#popup');
-    popup.innerHTML = names.join(', ');
-    popup.style.left = `${e.pageX}px`;
-    popup.style.top = `${e.pageY}px`;
-    popup.style.display = 'inline';
-    popup.style.position = 'absolute';
+  function getSite() {
+    return document.location.host.split('.')[0];
   }
 
-  function handleData(data, e) {
-    const favourites = data.favorites;
-    const names = favourites.map(favourite => favourite.user_name);
-    showPopup(names, e);
+  function hasSite(site) {
+    return [
+      'www', 'ask', 'metatalk', 'fanfare',
+      'projects', 'music', 'podcast'
+    ].includes(site);
   }
 
-  function addPopup() {
-    const html = '<div id="popup"></div>';
-    qs('body').insertAdjacentHTML('afterend', html);
+  function showVisualCue(comment, favouriteTotal, site) {
+    const width = ((favouriteTotal / 2) + 1);
+    comment.classList.add('highlight', `${site}color`);
+    comment.style.borderLeftWidth = `${width}px`;
+    comment.style.marginLeft = `${70 - width}px`;
   }
 
-  function removeTitles(favourites) {
-    favourites.forEach(function (favourite) {
-      favourite.removeAttribute('title');
-    });
-  }
-
-  function getEndpoint(href) {
-    const url = href.split('/');
-    const lid = url.pop();
-    const sid = url.pop();
-    return `https://www.metafilter.com/favorited/${sid}/${lid}/json`;
-  }
-
-  function showFavourites(e) {
-    fetch(getEndpoint(e.target.href))
-      .then(response => response.json())
-      .then(data => handleData(data, e))
-      .catch(handleError);
-  }
-
-  function hideFavourites() {
-    qs('#popup').style.display = 'none';
-    clearTimeout(timer);
-  }
-
-  function pauseHover(e) {
-    timer = setTimeout(function () {
-      showFavourites(e);
-    }, 300);
-  }
-
-  function getLinks() {
-    return qsa('span[id^="favcnt"] a');
-  }
-
-  function addListeners(favourites) {
-    favourites.forEach(function (favourite) {
-      favourite.addEventListener('mouseover', pauseHover, false);
-      favourite.addEventListener('mouseout', hideFavourites, false);
+  function getComments(site) {
+    qsa('.comments').forEach(function (comment) {
+      const favouriteTotal = getFavouriteTotal(comment);
+      if (favouriteTotal > 2) {
+        showVisualCue(comment, favouriteTotal, site);
+      }
     });
   }
 
   function init() {
-    addPopup();
-    const favourites = getLinks();
-    removeTitles(favourites);
-    addListeners(favourites);
+    const site = getSite();
+    if (hasSite(site)) getComments(site);
   }
 
   init();
